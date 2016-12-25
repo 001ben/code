97fbbdac5937727fffddc405a250ac238733c40e
brownb4 <benjamen.brown@cba.com.au>
2016-12-25 16:56:52 +1100

    Added basic example of svg and div bar graph

--- D3js/tutorials/1-fundamentals/index.js
+++ D3js/tutorials/1-fundamentals/index.js
@@ -1,7 +1,9 @@
-var dataset = [ 5, 10, 15, 20, 25 ];
+//var dataset = [...new Array(25).keys()].map(i=>Math.round(i*Math.random()*15+15));
+var dataset = [15, 21, 21, 45, 51, 37, 92, 65, 93, 55, 130, 144, 66, 162, 163, 239, 157, 164, 110, 65, 183, 147, 243, 121, 198]
 
-d3.select('body').selectAll('p')
+d3.select('body').selectAll('div')
   .data(dataset)
   .enter()
-  .append('p')
-  .text(d => d);
+  .append('div')
+  .attr('class', 'bar')
+  .style('height', d => d + 'px');
