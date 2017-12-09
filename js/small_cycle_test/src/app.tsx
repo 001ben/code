a5f81a9fc65da95ed6ecad0eb47903c747ba95ca
Benjamen Brown <Benjamen.Brown@cba.com.au>
2017-12-10 00:54:01 +1100

    Adding simple reactive stream

--- js/small_cycle_test/src/app.tsx
+++ js/small_cycle_test/src/app.tsx
@@ -2,11 +2,12 @@ import xs from 'xstream';
 import {ISinks, ISources} from './interfaces';
 
 export function App(sources: ISources): ISinks {
-  const vtree$ = xs.of(
-    <div>My Awesome Cycle.js app</div>,
+  // const vtree$ = xs.of(
+    // <div>My Awesome Cycle.js app</div>,
+  // );
+  const vtree$ = xs.periodic(1000).map((i) =>
+    <h1>{i} seconds have elapsed</h1>,
   );
-  // const vtree$ = xs.periodic(1000).map(i =>
-  // )
 
   return {
     DOM: vtree$,
